b5913772d3e359fa26e074adaae9593b64413918
raul_cardenas16 <raulcarfer@gmail.com>
2022-01-09 20:54:41 -0500

    Debugging

---
+++ src/debug/index.js
@@ -0,0 +1,11 @@
+var a = 'hello';
+function hello() {
+    let b = 'hello World';
+    const c =  'hello World!';
+    if (true) {
+        let d = 'hello World!!'
+        debugger
+    }
+}
+
+hello();
